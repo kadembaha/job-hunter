fba1a9af658f85271b4451534c2519fb8765246d
kadem <chekaktabaha.14@gmail.com>
2021-08-26 06:23:11 -0700

    completed the index html and style css

---
+++ js/script.js
@@ -0,0 +1,5 @@
+let findJob = document.querySelector('.button-container');
+findJob.addEventListener('click',()=>{
+    let text = document.getElementById('filter-jobs').value;
+    console.log(text);
+});
